a86b121e1648ed8bf9e1a8fc39712e4930dfacda
a-clmn <aleshiaclmn.work@gmail.com>
2023-03-15 15:53:38 -0700

    Added some events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//use 'mouseover' to change color of nav-links
+
+const nav = document.querySelectorAll(".nav-link");
+
+nav[0].addEventListener('mouseover', changeBGColor => 
+{changeBGColor.target.style.backgroundColor = "#add8e6"});
+
+nav[1].addEventListener('mouseover', changeBGColor => 
+{changeBGColor.target.style.backgroundColor = "#add8e6"});
+
+nav[2].addEventListener('mouseover', changeBGColor => 
+{changeBGColor.target.style.backgroundColor = "#add8e6"});
+
+nav[3].addEventListener('mouseover', changeBGColor => 
+{changeBGColor.target.style.backgroundColor = "#add8e6"});
+
+//use 'dblclick' to highlight p text
+
+const text = document.querySelectorAll("p");
+
+text[0].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[1].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[2].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[3].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[4].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[5].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[6].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[7].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+text[8].addEventListener('dblclick', highlight =>
+{highlight.target.style.backgroundColor = "yellow"});
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+//
